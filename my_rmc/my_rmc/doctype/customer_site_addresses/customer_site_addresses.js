86fa33f9858cd36b827ac79ca1e6220eb4d67fb0
ERP TECH <biswa4u85@gmail.com>
2025-09-05 12:40:51 +0530

    UPD

---
+++ my_rmc/my_rmc/doctype/customer_site_addresses/customer_site_addresses.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, my_rmc and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Customer Site Addresses', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ my_rmc/my_rmc/doctype/customer_site_addresses/customer_site_addresses.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, my_rmc and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class CustomerSiteAddresses(Document):
+	pass
